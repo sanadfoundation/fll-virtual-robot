43f9e6abb8736cfe6b90f41519c563a31d818752
Maher Gamal <maher.gamal@sanadfoundation.ca>
2026-05-02 16:47:47 -0400

    test: strip obsolete sensor/shadow stubs from JS window mock

--- tests/js/mocks/window.js
+++ tests/js/mocks/window.js
@@ -9,20 +9,9 @@ function makeWindowGlobals() {
   const window = {
     addEventListener: () => {},
     RobotSimulator: null,   // set by simulator.js at end of file
-    getColorSensorColor:        () => 'none',
-    getColorSensorReflection:   () => 50,
-    getColorSensorAmbient:      () => 30,
-    getColorSensorRGB:          () => [128, 128, 128],
-    getDistanceSensorValue:     () => 300,
-    getDistanceSensorPresence:  () => false,
-    getForceSensorValue:        () => 0,
-    getForceSensorPressed:      () => false,
-    getMotorSpeed:              () => 0,
-    getMotorPosition:           () => 0,
-    receiveCommands:            () => {},
-    appendOutput:               () => {},
-    AudioContext:               null,
-    webkitAudioContext:         null,
+    appendOutput:   () => {},
+    AudioContext:   null,
+    webkitAudioContext: null,
   };
 
   const document = {
